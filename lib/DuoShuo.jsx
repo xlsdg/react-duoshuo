fa70a96a7682cf2397ec840422edcb5a9796325e
xLsDg <xlsdg@qq.com>
2016-11-22 10:58:40 +0800

    refactor(mod): modify loading

--- lib/DuoShuo.jsx
+++ lib/DuoShuo.jsx
@@ -1,41 +1,67 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-export default class DuoShuo extends React.Component {
-  static propTypes = {
-    thread: React.PropTypes.string,
-    title: React.PropTypes.string,
-    url: React.PropTypes.string
-  }
-  static defaultProps = {
-    thread: '',
-    title: '',
-    url: ''
-  }
+class DuoShuo extends React.Component {
   constructor(props) {
+    // console.log('constructor', props);
     super(props);
+    this.state = {};
+    this._init = this._init.bind(this);
   }
-  _freshDOM() {
-    try {
-      DUOSHUO.EmbedThread(ReactDOM.findDOMNode(this));
-    } catch (e) {
-      DUOSHUO.EmbedThread(ReactDOM.findDOMNode(this));
-    }
+  _init() {
+    // console.log('_init');
+    let that = this;
+    window.DUOSHUO.EmbedThread(ReactDOM.findDOMNode(that));
+  }
+  componentWillMount() {
+    // console.log('componentWillMount');
   }
   componentDidMount() {
-    this._freshDOM();
+    // console.log('componentDidMount');
+    if (this.props.thread) {
+      this._init();
+    }
   }
-  componentDidUpdate() {
-    this._freshDOM();
+  componentWillReceiveProps(nextProps) {
+    // console.log('componentWillReceiveProps', nextProps);
+  }
+  shouldComponentUpdate(nextProps, nextState) {
+    // console.log('shouldComponentUpdate', nextProps, nextState);
+    return nextProps.thread !== this.props.thread;
+  }
+  componentWillUpdate(nextProps, nextState) {
+    // console.log('componentWillUpdate', nextProps, nextState);
+  }
+  componentDidUpdate(prevProps, prevState) {
+    // console.log('componentDidUpdate', prevProps, prevState);
+    if (this.props.thread) {
+      this._init();
+    }
+  }
+  componentWillUnmount() {
+    // console.log('componentWillUnmount');
   }
   render() {
+    // console.log('render');
     return (
       <div
+        key="duo-shuo"
         className="ds-thread"
         data-thread-key={this.props.thread}
         data-title={this.props.title}
         data-url={this.props.url}
-      />
+      ></div>
     );
   }
 }
+
+DuoShuo.propTypes = {
+    thread: React.PropTypes.string.isRequired,
+    title: React.PropTypes.string.isRequired,
+    url: React.PropTypes.string.isRequired
+};
+
+DuoShuo.defaultProps = {
+};
+
+export default DuoShuo;
